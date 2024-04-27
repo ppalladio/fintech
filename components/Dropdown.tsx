f198c0b0075b974c128e12ac4c6bb6397c4516f0
ppalladio <nine.slide@gmail.com>
2024-04-27 22:41:54 +0200

    finishing dropdown with zeego

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -5,13 +5,45 @@ export function Dropdown() {
     return (
         <DropdownMenu.Root>
             <DropdownMenu.Trigger>
-                <RoundBtn icon={'ellipse-horizontal'} text="More" />
+                <RoundBtn icon={'ellipsis-horizontal'} text="More" />
             </DropdownMenu.Trigger>
 
             <DropdownMenu.Content>
+                
                 <DropdownMenu.Item key="Statement">
                     <DropdownMenu.ItemTitle>Statement</DropdownMenu.ItemTitle>
-					
+                    <DropdownMenu.ItemIcon
+                        ios={{
+                            name: 'list.bullet.rectangle.fill',
+                            pointSize: 24,
+                        }}
+                    />
+                </DropdownMenu.Item>
+                <DropdownMenu.Item key="Convert">
+                    <DropdownMenu.ItemTitle>Convert</DropdownMenu.ItemTitle>
+                    <DropdownMenu.ItemIcon
+                        ios={{
+                            name: 'coloncurrencysign.arrow.circlepath',
+                            pointSize: 24,
+                        }}
+                    />
+                </DropdownMenu.Item>
+
+                <DropdownMenu.Item key="background">
+                    <DropdownMenu.ItemTitle>Background</DropdownMenu.ItemTitle>
+                    <DropdownMenu.ItemIcon
+                        ios={{ name: 'photo.fill', pointSize: 24 }}
+                    />
+                </DropdownMenu.Item>
+
+                <DropdownMenu.Item key="account">
+                    <DropdownMenu.ItemTitle>Account</DropdownMenu.ItemTitle>
+                    <DropdownMenu.ItemIcon
+                        ios={{
+                            name: 'plus.rectangle.on.folder.fill',
+                            pointSize: 24,
+                        }}
+                    />
                 </DropdownMenu.Item>
             </DropdownMenu.Content>
         </DropdownMenu.Root>
