aa5bd2d6190f6a7a6420714fc55df0e80b181a19
ppalladio <nine.slide@gmail.com>
2024-04-29 18:00:23 +0200

    drag and drop widgets

--- app/(authenticated)/(tabs)/home.tsx
+++ app/(authenticated)/(tabs)/home.tsx
@@ -5,29 +5,30 @@ import { useBalanceStore } from '@/store/balanceStore';
 import { defaultStyles } from '@/constants/Styles';
 import { Ionicons } from '@expo/vector-icons';
 import { Divider } from '@ui-kitten/components';
+import WidgetList from '@/components/SortableList/WidgetList';
 const Home = () => {
     const { balance, runTransaction, transactions, clearTransactions } =
         useBalanceStore();
     const reversedTransactions = [...transactions].reverse();
     const onAddMoney = () => {
-        runTransaction({
+        const amount =
+            Math.floor(Math.random() * 100) * (Math.random() > 0.5 ? 1 : -1);
+        runTransaction({ 
             id: Math.random().toString(),
-            amount:
-                Math.floor(Math.random() * 1000) *
-                (Math.random() > 0.5 ? 1 : -1),
+            amount,
             date: new Date(),
-            title: 'Added Money',
+            title: amount > 0 ? 'Added Money' : 'Removed Money',
         });
     };
     return (
-        <ScrollView className="bg-background">
-            <View className="m-[80px] align-center">
+        <ScrollView className="bg-background flex-1">
+            <View className="my-[50px] align-center">
                 <View className="flex flex-row items-baseline justify-center">
                     <Text className="text-[60px] font-bold">{balance()}</Text>
                     <Text className="text-[30px] ml-2 font-semibold">€</Text>
                 </View>
             </View>
-            <View className="flex flex-row mx-auto items-center justify-center">
+            <View className="flex flex-row items-center justify-center">
                 <RoundBtn icon="add" text="Add Money" onPress={onAddMoney} />
                 <RoundBtn
                     icon="refresh"
@@ -40,15 +41,15 @@ const Home = () => {
             <Text style={[defaultStyles.sectionHeader]} className="pb-5">
                 Transaction History
             </Text>
-            <View className=" p-[10px] bg-zinc-300 rounded-2xl gap-5">
+            <View className=" bg-zinc-200 gap-5 flex justify-center">
                 {transactions.length === 0 && (
-                    <Text className="text-slate-300 p-3">
+                    <Text className="text-slate-500 pl-5 my-3">
                         No Transactions Yet
                     </Text>
                 )}
                 {reversedTransactions.map((transaction) => (
                     <View key={transaction.id}>
-                        <View className="flex flex-row gap-x-3 items-center mb-1">
+                        <View className="flex flex-row gap-x-3 items-center my-1 pl-5">
                             <View>
                                 <Ionicons
                                     name={
@@ -62,22 +63,27 @@ const Home = () => {
                                     }
                                 />
                             </View>
-                            <View>
+                            <View className="text-center">
                                 <Text className="font-semibold">
                                     {transaction.title}
                                 </Text>
-                                <Text className="text-slate-500 text-sm">
+                                <Text className="text-slate-500 text-sm ">
                                     {transaction.date.toLocaleString()}
                                 </Text>
                             </View>
-                            <View>
-                                <Text>{transaction.amount}</Text>
+                            <View className="right-3 absolute flex-row justify-center items-baseline">
+                                <Text className="text-lg font-semibold">
+                                    {transaction.amount}{' '}
+                                </Text>
+                                <Text>€</Text>
                             </View>
                         </View>
-                        <Divider />
+                        <Divider className="font-bold" />
                     </View>
                 ))}
             </View>
+			<Text style={[defaultStyles.sectionHeader]}>Widgets</Text>
+			<WidgetList/>
         </ScrollView>
     );
 };
